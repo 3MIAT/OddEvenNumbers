2d307f52561f1968963480c21842c5a656b56618
MahmoudMagdy <mahmoudmagdyteama@gmail.com>
2026-02-05 02:07:26 +0200

    Initial commit

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+Console.WriteLine("Hello, World!");
+
+foreach (var n in numbers)
+{
+  Console.WriteLine($"{n}");
+}
